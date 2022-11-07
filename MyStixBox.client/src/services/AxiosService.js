d91acfd94f92f1f827ae37f1c80b57f9841c44dd
JeradeaSimmons <hippeejd@gmail.com>
2022-11-07 16:16:58 -0700

    api

--- MyStixBox.client/src/services/AxiosService.js
+++ MyStixBox.client/src/services/AxiosService.js
@@ -4,3 +4,19 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+const options = {
+  method: 'GET',
+  url: 'https://cigars.p.rapidapi.com/cigars',
+  params: {page: '1'},
+  headers: {
+    'X-RapidAPI-Key': '5e47072081msh25c96270e87412ep1764efjsn778ba99ce8c2',
+    'X-RapidAPI-Host': 'cigars.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
